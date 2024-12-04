804dbca70de11bd6644564622ace53c8ea791ee2
Ajinkya Aher <ajinkya.aher@esds.co.in>
2024-12-04 19:02:04 +0530

    Added genres wise filtering to the list.

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,8 +1,10 @@
 class Constants {
   static const String baseURL = "https://api.themoviedb.org/3/";
-  static const String trendingMovies = "trending/movie/day?language=en-US";
-  static const String topRatedMovies = "movie/top_rated";
-  static const String upcomingMovies = "movie/upcoming";
+  static const String trendingMovies = "trending/movie/day?include_adult=true&language=en-US";
+  static const String topRatedMovies = "movie/top_rated?include_adult=true";
+  static const String upcomingMovies = "movie/upcoming?include_adult=true";
+  static const String moviesGenre = "genre/movie/list";
+  static const String filterMovies = "discover/movie?with_genres=";
 
   // static const String loginScreen = "login";
   // static const String otpScreen = "otp_screen";
--- lib/data/dataproviders/movie_data_provider.dart
+++ lib/data/dataproviders/movie_data_provider.dart
@@ -1,3 +1,4 @@
+import 'package:eMeet/data/models/genre_list.dart';
 import 'package:eMeet/data/models/trending_movies.dart';
 
 import '../../config/constants.dart';
@@ -12,6 +13,12 @@ class MovieDataProvider {
     return trendingMovies;
   }
 
+  Future<TrendingMovies?> filterMoviesByGenre(String genreId) async {
+    final result = await APIService().getMethod("${Constants.filterMovies}$genreId");
+    TrendingMovies trendingMovies = TrendingMovies.fromJson(result ?? "");
+    return trendingMovies;
+  }
+
   Future<TopRatedMovies> getTopMovie() async {
     final result = await APIService().getMethod(Constants.topRatedMovies);
     TopRatedMovies topRatedMovies = TopRatedMovies.fromJson(result ?? "");
@@ -22,5 +29,11 @@ class MovieDataProvider {
     final result = await APIService().getMethod(Constants.trendingMovies);
     UpcomingMovies upcomingMovies = UpcomingMovies.fromJson(result ?? "");
     return upcomingMovies;
+  }
+
+  Future<GenreList> fetchMovieGenre() async {
+    final result = await APIService().getMethod(Constants.moviesGenre);
+    GenreList genreList = GenreList.fromJson(result ?? "");
+    return genreList;
   }
 }
